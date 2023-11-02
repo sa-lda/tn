6b669c28e4bf49ca8d2ab2414008ed6435dc9119
sa-lda <85459043+sa-lda@users.noreply.github.com>
2023-11-02 23:07:40 +0000

    Update api.js

--- my_functions/api.js
+++ my_functions/api.js
@@ -5,18 +5,21 @@ exports.handler = async function (event, context) {
   const response = await fetch('https://movies2watch.tv');
   const data = await response.json();
 
-
   
-            
-              return {
-                statusCode: 200,
-                headers: {
-                    'Access-Control-Allow-Origin': '*',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    message: data
+            return new Promise(async (resolve, reject) => {            
+
+                
+                resolve({
+                    statusCode: 200,
+                    headers: {
+                        'Access-Control-Allow-Origin': '*',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                      message: data
+                    })
                 })
-              }
+            
+            });
     
 }
